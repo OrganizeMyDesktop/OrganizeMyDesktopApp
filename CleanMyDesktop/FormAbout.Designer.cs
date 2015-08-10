b5c6b679f05563b47b64e3e6696cf6caeb6fa153
MCKanpolat <mehmetcan@kanpolat.net>
2015-08-11 00:50:49 +0300

    about form

---
+++ CleanMyDesktop/FormAbout.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CleanMyDesktop
+{
+	partial class FormAbout
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
+			this.okButton = new System.Windows.Forms.Button();
+			this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.textBoxDescription = new System.Windows.Forms.TextBox();
+			this.labelCopyright = new System.Windows.Forms.Label();
+			this.labelVersion = new System.Windows.Forms.Label();
+			this.labelProductName = new System.Windows.Forms.Label();
+			this.logoPictureBox = new System.Windows.Forms.PictureBox();
+			this.tableLayoutPanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// okButton
+			// 
+			resources.ApplyResources(this.okButton, "okButton");
+			this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.okButton.Name = "okButton";
+			// 
+			// tableLayoutPanel
+			// 
+			resources.ApplyResources(this.tableLayoutPanel, "tableLayoutPanel");
+			this.tableLayoutPanel.Controls.Add(this.logoPictureBox, 0, 0);
+			this.tableLayoutPanel.Controls.Add(this.labelProductName, 1, 0);
+			this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 1);
+			this.tableLayoutPanel.Controls.Add(this.labelCopyright, 1, 2);
+			this.tableLayoutPanel.Controls.Add(this.textBoxDescription, 1, 4);
+			this.tableLayoutPanel.Controls.Add(this.okButton, 1, 5);
+			this.tableLayoutPanel.Name = "tableLayoutPanel";
+			// 
+			// textBoxDescription
+			// 
+			resources.ApplyResources(this.textBoxDescription, "textBoxDescription");
+			this.textBoxDescription.Name = "textBoxDescription";
+			this.textBoxDescription.ReadOnly = true;
+			this.textBoxDescription.TabStop = false;
+			// 
+			// labelCopyright
+			// 
+			resources.ApplyResources(this.labelCopyright, "labelCopyright");
+			this.labelCopyright.Name = "labelCopyright";
+			// 
+			// labelVersion
+			// 
+			resources.ApplyResources(this.labelVersion, "labelVersion");
+			this.labelVersion.Name = "labelVersion";
+			// 
+			// labelProductName
+			// 
+			resources.ApplyResources(this.labelProductName, "labelProductName");
+			this.labelProductName.Name = "labelProductName";
+			// 
+			// logoPictureBox
+			// 
+			resources.ApplyResources(this.logoPictureBox, "logoPictureBox");
+			this.logoPictureBox.Name = "logoPictureBox";
+			this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 6);
+			this.logoPictureBox.TabStop = false;
+			// 
+			// FormAbout
+			// 
+			this.AcceptButton = this.okButton;
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.Controls.Add(this.tableLayoutPanel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FormAbout";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.tableLayoutPanel.ResumeLayout(false);
+			this.tableLayoutPanel.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button okButton;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+		private System.Windows.Forms.PictureBox logoPictureBox;
+		private System.Windows.Forms.Label labelProductName;
+		private System.Windows.Forms.Label labelVersion;
+		private System.Windows.Forms.Label labelCopyright;
+		private System.Windows.Forms.TextBox textBoxDescription;
+
+	}
+}
