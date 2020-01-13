eef9162e61ad5ff752a6acadcb26364c7332c724
Mehmet Kanpolat <mehmetcan@kanpolat.net>
2020-01-14 01:00:51 +0300

    FileSystemWatch improvoment

--- src/CleanMyDesktop.Core/IO/FileSystemWatch.cs
+++ src/CleanMyDesktop.Core/IO/FileSystemWatch.cs
@@ -3,57 +3,85 @@ using System.IO;
 
 namespace CleanMyDesktop.Core.IO
 {
-    public class FileSystemWatch : IDisposable
-	{
-		Lazy<FileSystemWatcher> _watchFolder;
-		string _path;
-		NotifyFilters _filter;
-		public FileSystemWatch(string path, NotifyFilters filter)
-		{
-			_watchFolder = new Lazy<FileSystemWatcher>();
-			_path = path;
-			_filter = filter;
-		}
+    public class FileSystemWatch : IFileSystemWatch
+    {
+        private readonly Lazy<FileSystemWatcher> _watchFolder;
+        private readonly string _path;
+        private readonly NotifyFilters _filter;
+        private readonly bool _includeSubdirectories;
 
-		public event FileSystemEventHandler Changed;
-		public event FileSystemEventHandler Created;
-		public event FileSystemEventHandler Deleted;
-		public event RenamedEventHandler Renamed;
-		public event ErrorEventHandler Error;
+        public FileSystemWatch(string path, NotifyFilters filter, bool includeSubdirectories = true)
+        {
+            _watchFolder = new Lazy<FileSystemWatcher>(() =>
+            {
+                var fileSystemWatcher = new FileSystemWatcher();
+                if (Changed != null)
+                    fileSystemWatcher.Changed += Changed;
+                if (Created != null)
+                    fileSystemWatcher.Created += Created;
+                if (Deleted != null)
+                    fileSystemWatcher.Deleted += Deleted;
+                if (Renamed != null)
+                    fileSystemWatcher.Renamed += Renamed;
+                if (Error != null)
+                    fileSystemWatcher.Error += Error;
+                fileSystemWatcher.IncludeSubdirectories = _includeSubdirectories;
+                return fileSystemWatcher;
+            });
+            _path = path;
+            _filter = filter;
+            _includeSubdirectories = includeSubdirectories;
+        }
 
-		public void Start()
-		{
-			_watchFolder.Value.Path = _path;
-			_watchFolder.Value.NotifyFilter = _filter;
-			_watchFolder.Value.Changed += Changed;
-			_watchFolder.Value.Created += Created;
-			_watchFolder.Value.Deleted += Deleted;
-			_watchFolder.Value.Renamed += Renamed;
-			_watchFolder.Value.Error += Error;
-			_watchFolder.Value.EnableRaisingEvents = true;
-		}
+        public event FileSystemEventHandler Changed;
+        public event FileSystemEventHandler Created;
+        public event FileSystemEventHandler Deleted;
+        public event RenamedEventHandler Renamed;
+        public event ErrorEventHandler Error;
 
-		public void Stop()
-		{
-			if (_watchFolder.IsValueCreated)
-			{
-				_watchFolder.Value.EnableRaisingEvents = false;
-			}
-		}
+        public void Start()
+        {
+            _watchFolder.Value.Path = _path;
+            _watchFolder.Value.NotifyFilter = _filter;
+            _watchFolder.Value.EnableRaisingEvents = true;
+        }
 
-		public bool Enabled
-		{
-			get { return _watchFolder.IsValueCreated && _watchFolder.Value.EnableRaisingEvents; }
-		}
+        public void Stop()
+        {
+            if (_watchFolder.IsValueCreated)
+            {
+                _watchFolder.Value.EnableRaisingEvents = false;
+            }
+        }
 
-		public void Dispose()
-		{
-			if (_watchFolder.IsValueCreated)
-			{
-				_watchFolder.Value.EnableRaisingEvents = false;
-				_watchFolder.Value.Dispose();
-			}
-			GC.SuppressFinalize(this);
-		}
-	}
+        public bool Enabled
+        {
+            get { return _watchFolder.IsValueCreated && _watchFolder.Value.EnableRaisingEvents; }
+        }
+
+        public bool IncludeSubdirectories
+        {
+            get { return _watchFolder.IsValueCreated && _watchFolder.Value.IncludeSubdirectories; }
+        }
+
+        public void Dispose()
+        {
+            if (_watchFolder.IsValueCreated)
+            {
+                _watchFolder.Value.EnableRaisingEvents = false;
+                if (Changed != null)
+                    _watchFolder.Value.Changed -= Changed;
+                if (Created != null)
+                    _watchFolder.Value.Created -= Created;
+                if (Deleted != null)
+                    _watchFolder.Value.Deleted -= Deleted;
+                if (Renamed != null)
+                    _watchFolder.Value.Renamed -= Renamed;
+                if (Error != null)
+                    _watchFolder.Value.Error -= Error;
+                _watchFolder.Value.Dispose();
+            }
+            GC.SuppressFinalize(this);
+        }
+    }
 }
---
+++ src/CleanMyDesktop.Core/IO/IFileSystemWatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace CleanMyDesktop.Core.IO
+{
+    public interface IFileSystemWatch : IDisposable
+    {
+        bool Enabled { get; }
+        bool IncludeSubdirectories { get; }
+
+        event FileSystemEventHandler Changed;
+        event FileSystemEventHandler Created;
+        event FileSystemEventHandler Deleted;
+        event ErrorEventHandler Error;
+        event RenamedEventHandler Renamed;
+
+        void Start();
+        void Stop();
+    }
+}
