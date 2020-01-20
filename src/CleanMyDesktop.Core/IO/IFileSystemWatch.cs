f9cec3e26b63039871bbbb5ae602176f82bcdff4
Mehmet Kanpolat <mehmetcan@kanpolat.net>
2020-01-20 23:31:53 +0300

    FileSystemWatch integration tests

--- src/CleanMyDesktop.Core/IO/IFileSystemWatch.cs
+++ src/CleanMyDesktop.Core/IO/IFileSystemWatch.cs
@@ -7,13 +7,15 @@ namespace CleanMyDesktop.Core.IO
     {
         bool Enabled { get; }
         bool IncludeSubdirectories { get; }
+        string Path { get; }
+        string Filter { get; }
+        NotifyFilters NotificationFilter { get; }
 
         event FileSystemEventHandler Changed;
         event FileSystemEventHandler Created;
         event FileSystemEventHandler Deleted;
         event ErrorEventHandler Error;
         event RenamedEventHandler Renamed;
-
         void Start();
         void Stop();
     }
