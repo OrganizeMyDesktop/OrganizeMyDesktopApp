ad46bcb4432ea2a78c717b9c2a0a78eca499d310
Mehmet Kanpolat <mehmetcan@kanpolat.net>
2020-01-20 23:31:53 +0300

    FileSystemWatch integration tests

--- src/CleanMyDesktop.Core/IO/FileSystemWatch.cs
+++ src/CleanMyDesktop.Core/IO/FileSystemWatch.cs
@@ -5,30 +5,17 @@ namespace CleanMyDesktop.Core.IO
 {
     public class FileSystemWatch : IFileSystemWatch
     {
-        private readonly Lazy<FileSystemWatcher> _watchFolder;
-        private readonly string _path;
-        private readonly NotifyFilters _filter;
-        private readonly bool _includeSubdirectories;
+        private bool initialized = false;
+        private FileSystemWatcher _watchFolder;
+        private string _path { get; set; }
+        private string _filter { get; set; }
+        private NotifyFilters _notificationFilter { get; set; }
+        private bool _includeSubdirectories { get; set; }
 
-        public FileSystemWatch(string path, NotifyFilters filter, bool includeSubdirectories = true)
+        public FileSystemWatch(string path, string filter, NotifyFilters notificationFilter, bool includeSubdirectories = true)
         {
-            _watchFolder = new Lazy<FileSystemWatcher>(() =>
-            {
-                var fileSystemWatcher = new FileSystemWatcher();
-                if (Changed != null)
-                    fileSystemWatcher.Changed += Changed;
-                if (Created != null)
-                    fileSystemWatcher.Created += Created;
-                if (Deleted != null)
-                    fileSystemWatcher.Deleted += Deleted;
-                if (Renamed != null)
-                    fileSystemWatcher.Renamed += Renamed;
-                if (Error != null)
-                    fileSystemWatcher.Error += Error;
-                fileSystemWatcher.IncludeSubdirectories = _includeSubdirectories;
-                return fileSystemWatcher;
-            });
             _path = path;
+            _notificationFilter = notificationFilter;
             _filter = filter;
             _includeSubdirectories = includeSubdirectories;
         }
@@ -39,47 +26,78 @@ namespace CleanMyDesktop.Core.IO
         public event RenamedEventHandler Renamed;
         public event ErrorEventHandler Error;
 
+        public string Path { get { return _path; } }
+        public string Filter { get { return _filter; } }
+        public NotifyFilters NotificationFilter { get { return _notificationFilter; } }
+
         public void Start()
         {
-            _watchFolder.Value.Path = _path;
-            _watchFolder.Value.NotifyFilter = _filter;
-            _watchFolder.Value.EnableRaisingEvents = true;
+            if (!initialized)
+            {
+                _watchFolder = new FileSystemWatcher(_path, _filter)
+                {
+                    IncludeSubdirectories = _includeSubdirectories,
+                    NotifyFilter = _notificationFilter
+                };
+
+                if (Changed != null)
+                    _watchFolder.Changed += Changed;
+                if (Created != null)
+                    _watchFolder.Created += Created;
+                if (Deleted != null)
+                    _watchFolder.Deleted += Deleted;
+                if (Renamed != null)
+                    _watchFolder.Renamed += Renamed;
+                if (Error != null)
+                    _watchFolder.Error += Error;
+
+                initialized = true;
+            }
+            _watchFolder.EnableRaisingEvents = true;
         }
 
         public void Stop()
         {
-            if (_watchFolder.IsValueCreated)
+            if (initialized)
             {
-                _watchFolder.Value.EnableRaisingEvents = false;
+                _watchFolder.EnableRaisingEvents = false;
             }
         }
 
         public bool Enabled
         {
-            get { return _watchFolder.IsValueCreated && _watchFolder.Value.EnableRaisingEvents; }
+            get { return initialized && _watchFolder.EnableRaisingEvents; }
         }
 
         public bool IncludeSubdirectories
         {
-            get { return _watchFolder.IsValueCreated && _watchFolder.Value.IncludeSubdirectories; }
+            get { return initialized && _watchFolder.IncludeSubdirectories; }
+        }
+
+        private void DetachEvents()
+        {
+            if (_watchFolder != null)
+            {
+                if (Changed != null)
+                    _watchFolder.Changed -= Changed;
+                if (Created != null)
+                    _watchFolder.Created -= Created;
+                if (Deleted != null)
+                    _watchFolder.Deleted -= Deleted;
+                if (Renamed != null)
+                    _watchFolder.Renamed -= Renamed;
+                if (Error != null)
+                    _watchFolder.Error -= Error;
+            }
         }
 
         public void Dispose()
         {
-            if (_watchFolder.IsValueCreated)
+            Stop();
+            if (_watchFolder != null)
             {
-                _watchFolder.Value.EnableRaisingEvents = false;
-                if (Changed != null)
-                    _watchFolder.Value.Changed -= Changed;
-                if (Created != null)
-                    _watchFolder.Value.Created -= Created;
-                if (Deleted != null)
-                    _watchFolder.Value.Deleted -= Deleted;
-                if (Renamed != null)
-                    _watchFolder.Value.Renamed -= Renamed;
-                if (Error != null)
-                    _watchFolder.Value.Error -= Error;
-                _watchFolder.Value.Dispose();
+                DetachEvents();
+                _watchFolder.Dispose();
             }
             GC.SuppressFinalize(this);
         }
---
+++ tests/CleanMyDesktop.Core.Tests/IO/FileSystemIntegrationTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanMyDesktop.Core.IO.Tests
+{
+    public class FileSystemIntegrationTests : IDisposable
+    {
+        private readonly string testFolder;
+        public FileSystemIntegrationTests()
+        {
+            testFolder = Path.Combine(Path.GetDirectoryName(GetType().GetTypeInfo().Assembly.Location), "testfolder");
+            if (!Directory.Exists(testFolder)) Directory.CreateDirectory(testFolder);
+        }
+
+        private string CombineFileName(string fileName)
+        {
+            return Path.Combine(testFolder, fileName);
+        }
+
+        [Fact]
+        public void CreateNewFileTest()
+        {
+            IFileSystemWatch fileSystemWatch = new FileSystemWatch(testFolder, "*.txt", NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.DirectoryName, true);
+            using (var monitoredSubject = fileSystemWatch.Monitor())
+            {
+                var fileName = Guid.NewGuid().ToString() + ".txt";
+                fileSystemWatch.Start();
+                Task.Delay(1000);
+                File.WriteAllText(CombineFileName(fileName), Guid.NewGuid().ToString());
+                Task.Delay(1000);
+                monitoredSubject.Should().Raise("Created")
+                .WithArgs<FileSystemEventArgs>(args => args.ChangeType == WatcherChangeTypes.Created);
+            }
+
+        }
+
+        [Fact]
+        public void FileDeleteTest()
+        {
+            const string fileName = "testfiled.txt";
+            var fullDir = CombineFileName(fileName);
+            File.WriteAllText(fullDir, Guid.NewGuid().ToString());
+            IFileSystemWatch fileSystemWatch = new FileSystemWatch(testFolder, "*.txt", NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.DirectoryName, true);
+            using (var monitoredSubject = fileSystemWatch.Monitor())
+            {
+                fileSystemWatch.Start();
+                Task.Delay(1000);
+                File.Delete(fullDir);
+                Task.Delay(1000);
+                monitoredSubject.Should().Raise("Deleted")
+                .WithArgs<FileSystemEventArgs>(args => args.ChangeType == WatcherChangeTypes.Deleted);
+            }
+        }
+
+        [Fact]
+        public void FileChangeTest()
+        {
+            const string fileName = "testfilec.txt";
+            var fullDir = CombineFileName(fileName);
+            File.WriteAllText(fullDir, Guid.NewGuid().ToString());
+
+            using (var fileSystemWatch = new FileSystemWatch(testFolder, "*.txt", NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.Size
+                                 | NotifyFilters.DirectoryName, true))
+            using (var monitoredSubject = fileSystemWatch.Monitor())
+            {
+                fileSystemWatch.Start();
+                Task.Delay(1000);
+                File.AppendAllText(fullDir, Guid.NewGuid().ToString());
+                Task.Delay(100);
+                monitoredSubject.Should().Raise("Changed")
+                .WithArgs<FileSystemEventArgs>(args => args.Name == fileName && args.ChangeType == WatcherChangeTypes.Changed);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(testFolder))
+                Directory.Delete(testFolder, true);
+        }
+    }
+}
